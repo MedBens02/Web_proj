7b9e986dbeafa4b5e2de246b5b9ab489157d11c4
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-05-02 18:07:18 +0100

    Update admin.php

--- models/admin.php
+++ models/admin.php
@@ -4,15 +4,27 @@
 require_once("../database/DB.php");
 
 
-    class Admin {
+class Admin {
 
-        static public function loginAdmin($login){
-            $stmt = DB::connect()->prepare('SELECT * FROM admin WHERE login = ?');
-            $stmt->execute([$login]);
-            $user = $stmt->fetch(PDO::FETCH_OBJ);  //returns an object with each column as attribute and value.
-            return $user; //object from table
-            $stmt->close();
-            $stmt = null;
-        }
-
+    static public function loginAdmin($login){
+        $stmt = DB::connect()->prepare('SELECT * FROM admin WHERE login = ?');
+        $stmt->execute([$login]);
+        $user = $stmt->fetch(PDO::FETCH_OBJ);  //returns an object with each column as attribute and value.
+        return $user; //object from table
+        $stmt->close();
+        $stmt = null;
     }
+
+    static public function getAllProfs() {
+        $stmt = DB::connect()->prepare('SELECT * FROM professeurs');
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    static public function getAllEtudiants() {
+        $stmt = DB::connect()->prepare('SELECT * FROM etudiant');
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+}
